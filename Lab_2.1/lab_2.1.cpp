cb8768e8a5638ce200fe68f87490bc9aa085df12
sasha-1337 <kolymbia97@gmail.com>
2020-10-10 17:20:39 +0300

    first master commit

---
+++ Lab_2.1/lab_2.1.cpp
@@ -0,0 +1,29 @@
+// Lab_2.1.cpp
+// Микита Олександр Степанович
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 2.1
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double a; //вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	// double z2; // результат обчислення 2-го виразу
+
+	cout << " a = "; cin >> a;
+
+	z1 =  ( ((a + 2) / sqrt(2 * a)) - (a/(sqrt(2*a) + 2)) + (2.0/(a-sqrt(2*a))) ) * ( (sqrt(a) - sqrt(2)) / (a + 2) );
+	// z2 =  1.0 / (sqrt(a) + sqrt(2) );
+
+	cout << " z1 = " << z1 << endl;
+	// cout << " z2 = " << z2 << endl;
+
+	cin.get();
+	system("pause"); 
+	return 0;
+}
